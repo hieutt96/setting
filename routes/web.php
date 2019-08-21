f4434396465c42e29d2d25593322efb4637b0bf1
trantrung.hieu <trantrung.hieu@bigtreetc.com>
2019-08-21 11:29:59 +0700

    first commit setting

--- routes/web.php
+++ routes/web.php
@@ -12,17 +12,21 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layouts');
 });
 
 Route::get('/app-secret/create', 'AppSecretController@create')->name('app-secret.create');
+
 Route::post('/app-secret/create', 'AppSecretController@postCreate')->name('app-secret.post.create');
 
 Route::get('/recharge-type/create', 'RechargeTypeController@create')->name('recharge-type.create');
+
 Route::post('/recharge-type/create', 'RechargeTypeController@postCreate')->name('recharge-type.post.create');
 
 Route::get('/withdrawal-type/create', 'WithdrawalTypeController@create')->name('withdrawal-type.create');
+
 Route::post('/withdrawal-type/create', 'WithdrawalTypeController@postCreate')->name('withdrawal-type.post.create');
 
 Route::get('/txn-type/create', 'TxnTypeController@create')->name('txn-type.create');
+
 Route::post('/txn-type/create', 'TxnTypeController@postCreate')->name('txn-type.post.create');
